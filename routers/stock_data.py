89c1444a1c3357e8db0063be10c2c9339e7f0829
thefutureproject <thefutureproject.dev@gmail.com>
2024-03-11 01:00:01 +0530

    Updated latest stock price

--- routers/stock_data.py
+++ routers/stock_data.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
 import schemas
-import yfinance as yf
 from typing import List
+from .Stock_Market.current_data import get_current_stock_price
+import concurrent.futures
 
 
 router = APIRouter(
@@ -13,9 +14,7 @@ router = APIRouter(
 @router.post("/latest", response_model=List[schemas.Stock_Market_Ticker_Out])
 async def get_latest_stock_data(tickers: schemas.Stock_Market_Ticker):
     current_prices = []
-    for ticker in tickers.tickers:
-        ticker_yahoo = yf.Ticker(ticker)
-        data = ticker_yahoo.history()
-        last_quote = data['Close'].iloc[-1]
-        current_prices.append({"ticker": ticker, "last_quote": last_quote})
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(
+            get_current_stock_price, ticker, current_prices) for ticker in tickers.tickers]
     return current_prices
