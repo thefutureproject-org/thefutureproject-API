b97b7259cbc1005efb4aa02b7620356fe3d1ee92
thefutureproject <thefutureproject.dev@gmail.com>
2024-03-19 21:03:29 +0530

    added gfg 404

--- routers/GFG_Modules/problem_info.py
+++ routers/GFG_Modules/problem_info.py
@@ -15,25 +15,29 @@ def get_problem_data(title_slug: str):
 
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        json_data = soup.find('script', id='__NEXT_DATA__').string
-        json_data = json.loads(json_data)
-        problem_data = json_data['props']['pageProps']['initialState']['problemApi']['queries']
-        first_key = next(iter(problem_data))
-        first_value = problem_data[first_key]['data']
+        try:
+            json_data = soup.find('script', id='__NEXT_DATA__').string
+            json_data = json.loads(json_data)
+            problem_data = json_data['props']['pageProps']['initialState']['problemApi']['queries']
+            first_key = next(iter(problem_data))
+            first_value = problem_data[first_key]['data']
 
-        proble_details = {
-            "id": first_value['id'],
-            "problem_name": first_value['problem_name'],
-            "difficulty": first_value['difficulty'],
-            "marks": first_value['marks'],
-            "accuracy": first_value['accuracy'],
-            "submissions": first_value['all_submissions'],
-            "company_tags": first_value["tags"]["company_tags"],
-            "topic_tags": first_value["tags"]["topic_tags"],
-            "author": first_value["author"],
-            "problem_question": first_value["problem_question"]
+            proble_details = {
+                "id": first_value['id'],
+                "problem_name": first_value['problem_name'],
+                "difficulty": first_value['difficulty'],
+                "marks": first_value['marks'],
+                "accuracy": first_value['accuracy'],
+                "submissions": first_value['all_submissions'],
+                "company_tags": first_value["tags"]["company_tags"],
+                "topic_tags": first_value["tags"]["topic_tags"],
+                "author": first_value["author"],
+                "problem_question": first_value["problem_question"]
 
-        }
-        return proble_details
+            }
+            return proble_details
+        except:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="Problem not found")
     else:
         get_problem_data(title_slug)
