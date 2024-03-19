9c54067c8ed577ab17485159f8693b19608296f9
thefutureproject <thefutureproject.dev@gmail.com>
2024-03-19 22:52:07 +0530

    minor update

--- routers/leetcode.py
+++ routers/leetcode.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header
 from .Leetcode_Contest.contest_status import contest_status
 from .Leetcode_Contest.contest_info import contest_info
 from .Leetcode_Contest.leetcode_problem_info import get_problem_info
@@ -10,6 +10,7 @@ from fastapi import Depends
 from .Database import models
 from typing import List
 from sqlalchemy import func
+import random
 
 
 router = APIRouter(
@@ -24,13 +25,31 @@ async def get_contest_status():
 
 
 @router.post("/contest/ranking", response_model=List[schemas.Contest_Ranking_Out])
-async def get_contest_ranking(usernames: schemas.Contest_Ranking, db: Session = Depends(get_db)):
-    contests = db.query(models.Contest).filter(
-        func.lower(models.Contest.username).in_(
-            [username.lower() for username in usernames.contestants_ids]
-        )
-    ).all()
-    return contests
+async def get_contest_ranking(usernames: schemas.Contest_Ranking, origin: str = Header(None), db: Session = Depends(get_db)):
+    if (origin == "chrome-extension://mcfpimbkecfbccgdamgejenmljjcamce"):
+        contests = db.query(models.Contest).filter(
+            func.lower(models.Contest.username).in_(
+                [username.lower() for username in usernames.contestants_ids]
+            )
+        ).all()
+        return contests
+    else:
+        user_data = [
+            {
+                "username": username,
+                "rank": random.randint(1, 25000),
+                "finish_time": f"{random.randint(0, 1)}:{random.randint(0, 30)}:{random.randint(0, 60)}",
+                "A_st": f"{random.randint(0, 1)}:{random.randint(0, 30)}:{random.randint(0, 60)}",
+                "A_flc": random.randint(0, 2),
+                "B_st": f"{random.randint(0, 1)}:{random.randint(0, 30)}:{random.randint(0, 60)}",
+                "B_flc": random.randint(0, 4),
+                "C_st": f"{random.randint(0, 1)}:{random.randint(0, 30)}:{random.randint(0, 60)}",
+                "C_flc": random.randint(0, 6),
+                "D_st": f"{random.randint(0, 1)}:{random.randint(0, 30)}:{random.randint(0, 60)}",
+                "D_flc": random.randint(0, 8)
+            } for username in usernames.contestants_ids
+        ]
+        return user_data
 
 
 @router.post("/contest/info", response_model=schemas.Contest_Info_Out)
