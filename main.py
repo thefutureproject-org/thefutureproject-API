9cefb4d9131cc9b5997b4e82f44394cf4338f1dc
thefutureproject <thefutureproject.dev@gmail.com>
2024-03-20 22:17:18 +0530

    minor changes

--- main.py
+++ main.py
@@ -65,26 +65,26 @@ async def main():
     return {"message": "Welcome to The Future Project"}
 
 
-# @app.get("/docs", include_in_schema=False)
-# async def custom_swagger_ui_html():
-#     return get_swagger_ui_html(
-#         openapi_url=app.openapi_url,
-#         title=app.title,
-#         oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-#         swagger_js_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
-#         swagger_css_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css",
-#     )
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html():
+    return get_swagger_ui_html(
+        openapi_url=app.openapi_url,
+        title=app.title,
+        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
+        swagger_js_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui-bundle.js",
+        swagger_css_url="https://unpkg.com/swagger-ui-dist@5.9.0/swagger-ui.css",
+    )
 
 
-# @app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
-# async def swagger_ui_redirect():
-#     return get_swagger_ui_oauth2_redirect_html()
+@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
+async def swagger_ui_redirect():
+    return get_swagger_ui_oauth2_redirect_html()
 
 
-# @app.get("/redoc", include_in_schema=False)
-# async def redoc_html():
-#     return get_redoc_html(
-#         openapi_url=app.openapi_url,
-#         title=app.title,
-#         redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js"
-#     )
+@app.get("/redoc", include_in_schema=False)
+async def redoc_html():
+    return get_redoc_html(
+        openapi_url=app.openapi_url,
+        title=app.title,
+        redoc_js_url="https://unpkg.com/redoc@next/bundles/redoc.standalone.js"
+    )
